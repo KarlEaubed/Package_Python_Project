ffaca9a5b4fda9ab870f1893dd25bb238cfad28f
Carl Obed Esdras DORVIL <carlobeddorvil20@gmail.com>
2023-11-13 23:40:46 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from sjpgpng.convert import jpg_to_png, png_to_jpg
+
+
+if __name__ == "__main__" :
+    input_file = 'bg.jpg'
+    output_file = 'image.png'
+    jpg_to_png(input_file, output_file)
+    input_file1 = 'background.png'
+    output_file1 = 'image1.jpg'
+    png_to_jpg(input_file1, output_file1)
